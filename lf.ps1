5e7e186e1840e0d7a6c7c8ae32f4f192e25d5022
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-31 00:20:13 +0800

    Make powershell more "fancy" with changing title.

--- lf.ps1
+++ lf.ps1
@@ -1,1 +1,10 @@
+$prevTitle = $host.UI.RawUI.WindowTitle
+
+$isLFCD = $false
+foreach ($arg in $args) {
+    if ($arg -match '^-last-dir-path=') { $isLFCD = $true }
+}
+
+$host.UI.RawUI.WindowTitle = $isLFCD ? 'lfcd' : 'lf'
 &lf.bin.exe '-single' $args
+$host.UI.RawUI.WindowTitle = $prevTitle
