e09062db62221cf930f63a37be3dcbfab8e0d1bb
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-31 00:26:44 +0800

    changed command that run on lfcd

--- lfcd.ps1
+++ lfcd.ps1
@@ -20,7 +20,7 @@ if (Test-Path -PathType Leaf "$tmp") {
     Remove-Item -Force "$tmp"
     if (Test-Path -PathType Container "$dir") {
         if ("$dir" -ne "$pwd") {
-            cd "$dir"
+            Set-Location "$dir"
         }
     }
 }
